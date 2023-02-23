b298ffa4cdc451ff2c0a303bf60b9781e231b3ae
Kal1219 <bandidouniversal@gmail.com>
2023-02-23 15:51:15 -0600

    Initial Commit

---
+++ SwiftUIFireBaseAuth/SwiftUIFireBaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFireBaseAuthApp.swift
+//  SwiftUIFireBaseAuth
+//
+//  Created by Servicio Kaleb on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFireBaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
